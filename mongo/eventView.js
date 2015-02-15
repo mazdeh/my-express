59d26ef9a9fab2ae566dd29400cccf365a86bb2e
mazdeh <vahid.mazdeh@gmail.com>
2015-02-15 13:17:15 -0700

    50/50 https://my-express.herokuapp.com/list/repos

--- mongo/eventView.js
+++ mongo/eventView.js
@@ -3,11 +3,10 @@ module.exports = function(app) {
     app.get('/view/event/:id', function(req, res) {
 
         var events = app.db.get('events')
-
          var q = {
             'id': req.params.id
         }
-
+        
         var events = events.findOne(q, function(err, item) {
 
             res.render('viewEvent.jade', {
